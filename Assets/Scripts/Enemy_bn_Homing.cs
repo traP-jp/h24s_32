dab44336e3d0cce99b4ce2913b25b817ff3369b8
BN256 <blancnoir256@gmail.com>
2024-06-15 10:19:28 +0900

    homingの回数に上限を定めました

--- Assets/Scripts/Enemy_bn_Homing.cs
+++ Assets/Scripts/Enemy_bn_Homing.cs
@@ -9,6 +9,8 @@ public class Enemy_bn_Homing : MonoBehaviour
     private float time;
     public float freezTime;
     public float enemySpeed;
+    public int homingTimes;
+    public int maxHomingTimes;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,7 @@ public class Enemy_bn_Homing : MonoBehaviour
         rb.gravityScale = 0;
         rb.drag = 0;
         time = 0f;
+        homingTimes = 0;
     }
 
     // Update is called once per frame
@@ -25,15 +28,20 @@ public class Enemy_bn_Homing : MonoBehaviour
 
         if (time > freezTime)
         {
-            //一旦停止して打ち出す
-            rb.velocity = new Vector2(0, 0);
-            //freezTimeを超えるとenemyからplayerに向かって打ち出されるイメージ
-            Vector2 thisPosition = (Vector2)this.transform.position;
-            Vector2 otherPosition = (Vector2)player.transform.position;
-            Vector2 difVector = otherPosition - thisPosition;
-            Vector2 difVectorE = difVector.normalized;
-            rb.AddForce(difVectorE * enemySpeed, ForceMode2D.Impulse);
-            time = 0f;
+            homingTimes++;
+            if (homingTimes < maxHomingTimes)
+            {
+                //一旦停止して打ち出す
+                rb.velocity = new Vector2(0, 0);
+                //freezTimeを超えるとenemyからplayerに向かって打ち出されるイメージ
+                Vector2 thisPosition = (Vector2)this.transform.position;
+                Vector2 otherPosition = (Vector2)player.transform.position;
+                Vector2 difVector = otherPosition - thisPosition;
+                Vector2 difVectorE = difVector.normalized;
+                rb.AddForce(difVectorE * enemySpeed, ForceMode2D.Impulse);
+                time = 0f;
+            }
+
         }
     }
 }
