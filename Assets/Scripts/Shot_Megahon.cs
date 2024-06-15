9f84fee1710670b0f28d029df01de365f6679730
kamecha <masakikameyama81@gmail.com>
2024-06-16 07:52:18 +0900

    単純に移動させるようにした

--- Assets/Scripts/Shot_Megahon.cs
+++ Assets/Scripts/Shot_Megahon.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 子供ショットがこれを中心にぐるぐる周りながら進む
 public class Shot_Megahon : MonoBehaviour
 {
+    // oisu-1234が装填される予定
+    public GameObject[] shotPrefabs;
+    // 半径
+    public float radius = 1.0f;
+    private float speed;
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        speed = GetComponent<Shot>().speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rb.velocity = Vector2.up * speed;
     }
 }
