c2bd535af45a7a47f25aefd1716d1cd0e7f8b7f2
kamecha <masakikameyama81@gmail.com>
2024-06-15 11:32:57 +0900

    ホーミング処理を追加

--- Assets/Scripts/Shot_Homing.cs
+++ Assets/Scripts/Shot_Homing.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class Shot_Homing : MonoBehaviour
 {
+    private float speed;
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
+        speed = GetComponent<Shot>().speed;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Searchで見つけた敵に向かって移動する
+        Vector2 target = SearchEnemy().GetComponent<Transform>().position;
+        Vector2 direction = target - (Vector2)transform.position;
+        _rb.velocity = direction.normalized * speed;
     }
 
+    // 一番近くの敵を探す
+    private GameObject SearchEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject nearestEnemy = null;
+        float minDistance = float.MaxValue;
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestEnemy = enemy;
+            }
+        }
+        return nearestEnemy;
+    }
 }
