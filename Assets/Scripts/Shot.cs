3f6ce19850f6cc8b500dd4c95cb2fde971e79036
kamecha <masakikameyama81@gmail.com>
2024-06-15 11:32:35 +0900

    Shotで動かすとHomingが動かないからShotの該当箇所を削除

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -8,19 +8,13 @@ public class Shot : MonoBehaviour
 {
     public float damage;
     public float speed = 5.0f;
-    private Rigidbody2D _rb;
-    private Collider2D _collider;
     // Start is called before the first frame update
     void Start()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 常に上に移動する
-        _rb.velocity = Vector2.up * speed;
     }
 }
