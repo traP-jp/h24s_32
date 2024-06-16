f7f591d8fc5799051dde5b0052d8c2a08efdffdf
kamecha <masakikameyama81@gmail.com>
2024-06-16 13:32:41 +0900

    垂直弾生成時のおいスー

--- Assets/Scripts/Shot_Straight.cs
+++ Assets/Scripts/Shot_Straight.cs
@@ -6,11 +6,14 @@ public class Shot_Straight : MonoBehaviour
 {
     private float speed;
     private Rigidbody2D _rb;
+    private AudioSource _audio;
     // Start is called before the first frame update
     void Start()
     {
         speed = GetComponent<Shot>().speed;
-        _rb = GetComponent<Rigidbody2D>();   
+        _rb = GetComponent<Rigidbody2D>();
+        _audio = GetComponent<AudioSource>();
+        _audio.Play();
     }
 
     // Update is called once per frame
