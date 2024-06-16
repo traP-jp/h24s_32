0f5acb9189348745bbee988002680c2bbbd409af
BN256 <blancnoir256@gmail.com>
2024-06-16 14:23:34 +0900

    だいたい完成

--- Assets/Scripts/EnemyManagerStage_8.cs
+++ Assets/Scripts/EnemyManagerStage_8.cs
@@ -25,15 +25,15 @@ public class EnamyManagerStage_8 : MonoBehaviour
     private int[] counter;
     public int[] counterMax = new int[7];//enemyCount == counterMax.Len
     public float[] levelup = new float[5];//levelup.Length == levelの数;
-    private class RandomObjects{}
+    private class RandomObjects { }
     private Rigidbody2D fall;
     private FixedSpeed fixedSpeed;
     private ChangingAirResistance car;
     private ZigzagFall zig;
-    private Enemy_bn_Homing homing; 
+    private Enemy_bn_Homing homing;
     private Enemy_bn_path path;
     private Enemy_bn_random random;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -64,15 +64,13 @@ public class EnamyManagerStage_8 : MonoBehaviour
             time[i] += Time.deltaTime;
         }
         hp = player.currentHP;
-        hp = 2;
-        maxHp = 3;
         hpPercentage = hp / maxHp * 100;
         switch (hpPercentage)
         {
             case float threshold_value when threshold_value > 90:
                 for (int i = 0; i < time.Length; i++)
                 {
-                    if (time[i] > timer[i]*levelup[0])
+                    if (time[i] > timer[i] * levelup[0])
                     {
                         if (counter[i] < counterMax[i])
                         {
@@ -80,21 +78,22 @@ public class EnamyManagerStage_8 : MonoBehaviour
                             {
                                 Pop(enemys[i], RandPos());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                             else
                             {
                                 Pop(enemys[i], RandPosInField());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                         }
                     }
-                    time[i] = 0;
                 }
                 break;
             case float threshold_value when threshold_value > 75:
                 for (int i = 0; i < time.Length; i++)
                 {
-                    if (time[i] > timer[i]*levelup[1])
+                    if (time[i] > timer[i] * levelup[1])
                     {
                         if (counter[i] < counterMax[i])
                         {
@@ -102,21 +101,22 @@ public class EnamyManagerStage_8 : MonoBehaviour
                             {
                                 Pop(enemys[i], RandPos());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                             else
                             {
                                 Pop(enemys[i], RandPosInField());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                         }
                     }
-                    time[i] = 0;
                 }
                 break;
             case float threshold_value when threshold_value > 50:
                 for (int i = 0; i < time.Length; i++)
                 {
-                    timer_[i] = Random.Range(0f, timer[i]*levelup[2]);
+                    timer_[i] = Random.Range(0f, timer[i] * levelup[2]);
                     if (time[i] > timer_[i])
                     {
                         if (counter[i] < counterMax[i])
@@ -125,63 +125,68 @@ public class EnamyManagerStage_8 : MonoBehaviour
                             {
                                 Pop(enemys[i], RandPos());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                             else
                             {
                                 Pop(enemys[i], RandPosInField());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                         }
                     }
-                    time[i] = 0;
                 }
                 break;
             case float threshold_value when threshold_value > 30:
                 for (int i = 0; i < time.Length; i++)
                 {
-                    timer_[i] = Random.Range(0f, timer[i]*levelup[3]);
-                    RandomMaker();
+                    timer_[i] = Random.Range(0f, timer[i] * levelup[3]);
                     if (time[i] > timer_[i])
                     {
                         if (counter[i] < counterMax[i])
                         {
                             if (i < enemyTypeChange)
                             {
+                                RandomMaker();
                                 Pop(enemys[i], RandPos());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                             else
                             {
+                                RandomMaker();
                                 Pop(enemys[i], RandPosInField());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                         }
                     }
-                    time[i] = 0;
                 }
                 break;
             case float threshold_value when threshold_value > 10:
                 for (int i = 0; i < time.Length; i++)
                 {
-                    timer_[i] = Random.Range(0f, timer[i]*levelup[4]);
-                    RandomMaker();
+                    timer_[i] = Random.Range(0f, timer[i] * levelup[4]);
                     if (time[i] > timer_[i])
                     {
                         if (counter[i] < counterMax[i])
                         {
                             if (i < enemyTypeChange)
                             {
+                                RandomMaker();
                                 Pop(enemys[i], RandPos());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                             else
                             {
+                                RandomMaker();
                                 Pop(enemys[i], RandPosInField());
                                 counter[i]++;
+                                time[i] = 0;
                             }
                         }
                     }
-                    time[i] = 0;
                 }
                 break;
             default:
@@ -212,22 +217,22 @@ public class EnamyManagerStage_8 : MonoBehaviour
     }
     void RandomMaker()
     {
-        fall.gravityScale = Random.Range(1f,2f);
-        fall.drag = Random.Range(0f,2f);
-        fixedSpeed.targetVelocity = Random.Range(0.05f,0.2f);
-        car.Appeartime = Random.Range(0.5f,1.5f);
-        car.drag = Random.Range(6f,14f);
-        zig.swingRange = Random.Range(3f,7f);
-        zig.swingSpeed = Random.Range(5f,8f);
-        zig.speed = Random.Range(-9f,-7f);
-        homing.freezeTime = Random.Range(1f,4f);
-        homing.enemySpeed = Random.Range(4f,7f);
-        path.freezTime = Random.Range(0f,1.5f);
-        path.actStartTime = path.freezTime + Random.Range(0f,1f);
-        path.moveStartTime = path.actStartTime + Random.Range(2f,4f);
-        path.enemySpeed = Random.Range(8f,15f);
-        path.enemyAirResistance = Random.Range(0f,1.5f);
-        random.freezeTime = Random.Range(0f,1.5f);
-        random.maxLen = Random.Range(2f,5f);
+        fall.gravityScale = Random.Range(1f, 2f);
+        fall.drag = Random.Range(0f, 2f);
+        fixedSpeed.targetVelocity = Random.Range(0.05f, 0.2f);
+        car.Appeartime = Random.Range(0.5f, 1.5f);
+        car.drag = Random.Range(6f, 14f);
+        zig.swingRange = Random.Range(3f, 7f);
+        zig.swingSpeed = Random.Range(5f, 8f);
+        zig.speed = Random.Range(-9f, -7f);
+        homing.freezeTime = Random.Range(1f, 4f);
+        homing.enemySpeed = Random.Range(4f, 7f);
+        path.freezTime = Random.Range(0f, 1.5f);
+        path.actStartTime = path.freezTime + Random.Range(0f, 1f);
+        path.moveStartTime = path.actStartTime + Random.Range(2f, 4f);
+        path.enemySpeed = Random.Range(8f, 15f);
+        path.enemyAirResistance = Random.Range(0f, 1.5f);
+        random.freezeTime = Random.Range(0f, 1.5f);
+        random.maxLen = Random.Range(2f, 5f);
     }
 }
